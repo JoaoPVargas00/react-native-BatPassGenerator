8e3f9c4930d0d591116ddb3fbd38d497739e186d
João P.Vargas <joaopsvargas@hotmail.com>
2025-08-14 10:53:00 -0300

    Bat pass

---
+++ bat-pass-app/src/screens/Home/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import styles from "./Style";
+import { BatLogo } from "../../components/BatLogo/BatLogo";
+import { BattextInput } from "../../components/BatTextInput/BatTextInput";
+import { BatButton } from "../../components/BatButton/batButton";
+
+export default function Home() {
+    return (
+        <View style={styles.appContainer}>
+       
+       <View style={styles.logoContainer}>
+        <BatLogo/>
+       </View>
+
+       <View style={styles.inputContainer}>
+        <BatButton/>
+
+       </View>
+            <StatusBar style="light" />
+        </View>
+    );
+}
